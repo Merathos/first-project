0198bec45ffb860346c873c3950237c1842bef64
Artem Kartavtsev <arkartavtsev@live.ru>
2019-09-19 21:30:58 +0700

    Добавляет скрипты открытия меню пользователя

---
+++ source/js/modules/user-menu.js
@@ -0,0 +1,44 @@
+'use strict';
+
+
+(function () {
+  var menu = document.querySelector('.user-menu');
+
+
+  if (!menu) {
+    return;
+  }
+
+
+  var menuBtn = menu.querySelector('.user-menu__dropdown-toggle');
+  var overlay = menu.querySelector('.user-menu__overlay');
+  var list = menu.querySelector('.user-menu__list');
+
+
+  var onMenuBtnClick = function () {
+    menu.classList.toggle('user-menu--open');
+    overlay.classList.toggle('user-menu__overlay--shown');
+
+    list.style.height = (list.offsetHeight === 0) ? list.scrollHeight + 'px' : '';
+  };
+
+  var closeDropdown = function () {
+    menu.classList.remove('user-menu--open');
+    overlay.classList.remove('user-menu__overlay--shown');
+  };
+
+
+  menu.classList.add('user-menu--js');
+
+
+  menuBtn.addEventListener('click', onMenuBtnClick);
+  overlay.addEventListener('click', closeDropdown);
+
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === window.const.keyCode.ESC && overlay.classList.contains('user-menu__overlay--shown')) {
+      evt.preventDefault();
+
+      closeDropdown();
+    }
+  });
+})();
