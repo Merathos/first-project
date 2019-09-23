a6e86a19f31103e9585daa045eb1f819519a5deb
Artem Kartavtsev <arkartavtsev@live.ru>
2019-09-24 00:51:27 +0700

    Добавляет ограничение длины заголовков карточек проектов

--- build/js/main.js
+++ build/js/main.js
@@ -252,6 +252,25 @@
 
 
 (function () {
+  var list = document.querySelector('.projects__list');
+
+  if (!list) {
+    return;
+  }
+
+  var ellipsis = Ellipsis({
+    lines: 3
+  });
+
+  var titles = list.querySelectorAll('.project-item__title');
+
+  ellipsis.add(titles);
+})();
+
+'use strict';
+
+
+(function () {
   var menu = document.querySelector('.user-menu');
 
 
---
+++ source/js/modules/projects.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+(function () {
+  var list = document.querySelector('.projects__list');
+
+  if (!list) {
+    return;
+  }
+
+  var ellipsis = Ellipsis({
+    lines: 3
+  });
+
+  var titles = list.querySelectorAll('.project-item__title');
+
+  ellipsis.add(titles);
+})();
