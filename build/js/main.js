9089062baa80aebcbeb46a7c23b0bbe5d9def3d9
Artem Kartavtsev <arkartavtsev@live.ru>
2019-09-23 14:59:20 +0700

    Добавляет скрипты фильтра

--- build/js/main.js
+++ build/js/main.js
@@ -48,6 +48,38 @@ function reinitSelect($) {
 
 
 (function () {
+  var filter = document.querySelector('.filter');
+
+
+  if (!filter) {
+    return;
+  }
+
+
+  var openBtn = filter.querySelector('.filter__open-btn');
+  var body = filter.querySelector('.filter__body');
+
+
+  var onOpenBtnClick = function (evt) {
+    filter.classList.toggle('filter--open');
+
+    evt.currentTarget.textContent =
+      filter.classList.contains('filter--open') ?
+      'Скрыть фильтры'
+      :
+      'Фильтры';
+  };
+
+
+  filter.classList.add('filter--js');
+
+  openBtn.addEventListener('click', onOpenBtnClick);
+})();
+
+'use strict';
+
+
+(function () {
   var switcher = document.querySelector('.location-switcher');
 
 
