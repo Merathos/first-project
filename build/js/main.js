93ac04212de1b7b1f27f86e6811a6d8eb7a323d1
Damir92 <home_damir92@mail.ru>
2019-09-23 21:37:10 +0300

    Add button-show all

--- build/js/main.js
+++ build/js/main.js
@@ -217,6 +217,32 @@
 
 'use strict';
 
+(function () {
+  var hideClass = 'js_hide'
+  var list = document.querySelector('.js_short_list');
+  var button = document.querySelector('.js_short_list + .js_show_all');
+
+  if (list && button) {
+    // var showedItems = 5;
+    var items = list.querySelectorAll('li');
+
+    items.forEach(function (item, index) {
+      if (index > 4) {
+        item.classList.add(hideClass);
+      }
+    })
+
+    button.addEventListener('click', function () {
+      items.forEach(function (item) {
+        item.classList.remove(hideClass);
+        button.classList.add(hideClass);
+      })
+    })
+  }
+})();
+
+'use strict';
+
 
 (function () {
   var menu = document.querySelector('.user-menu');
