9d237c40ba0e543a981dee37c826942e6e02859c
Damir92 <home_damir92@mail.ru>
2019-09-25 09:11:19 +0300

    bigFix main_page

---
+++ source/js/modules/cookie.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function () {
+  var hideClass = 'js_hide'
+  var cookieBlock = document.querySelector('.js_cookie');
+  var button = document.querySelector('.js_cookie .js_btn');
+
+  if (cookieBlock && button) {
+    button.addEventListener('click', function () {
+      cookieBlock.classList.add(hideClass);
+    })
+  }
+})();
