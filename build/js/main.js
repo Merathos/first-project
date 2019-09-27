ef08bfe9e563cfb339f14dd59f1f360d63e98942
Artem Kartavtsev <arkartavtsev@live.ru>
2019-09-27 20:57:31 +0700

    Добавляет скрипт смены состояния кнопки "подписаться"

--- build/js/main.js
+++ build/js/main.js
@@ -328,6 +328,37 @@
 
 
 (function () {
+  var subscribeLink = document.querySelector('.social__link--subscribe, .social__link--unsubscribe');
+
+  if (!subscribeLink) {
+    return;
+  }
+
+
+  var onSubscribeLinkClick = function (evt) {
+    evt.preventDefault();
+
+    if (subscribeLink.classList.contains('social__link--subscribe')) {
+      subscribeLink.textContent = 'Отписаться';
+
+      subscribeLink.classList.remove('social__link--subscribe');
+      subscribeLink.classList.add('social__link--unsubscribe');
+    } else {
+      subscribeLink.textContent = 'Подписаться';
+
+      subscribeLink.classList.add('social__link--subscribe');
+      subscribeLink.classList.remove('social__link--unsubscribe');
+    }
+  };
+
+
+  subscribeLink.addEventListener('click', onSubscribeLinkClick);
+})();
+
+'use strict';
+
+
+(function () {
   var upBtn = document.querySelector('.page-body__up-btn');
 
   if (!upBtn) {
