ba61d62fe985413651a806f2e02e9a98f821073b
AnVP <amltrp@mail.ru>
2019-09-28 18:26:27 +0500

    Добавляет слайдер

--- build/js/main.js
+++ build/js/main.js
@@ -246,18 +246,55 @@
   ellipsis.add(titles);
 })();
 
-// var rangeSlider = document.getElementById('slider-range');
+'use strict';
 
-// noUiSlider.create(rangeSlider, {
-//     start: [5],
-//     behaviour: 'snap',
-//     connect: 'lower',
-//     step: 1,
-//     range: {
-//         'min': [1],
-//         'max': [10]
-//     }
-// });
+(function () {
+  var rangeSlider = document.getElementById('poll-range');
+  if (!rangeSlider) {
+    return;
+  }
+
+  noUiSlider.create(rangeSlider, {
+    start: [5],
+    behaviour: 'snap',
+    connect: 'lower',
+    step: 1,
+    padding: [1, 0],
+    range: {
+      'min': [0],
+      'max': [10]
+    },
+    pips: {
+      mode: 'steps',
+      stepped: true,
+      density: 4
+    },
+    tooltips: true,
+    format: wNumb({
+      decimals: 0,
+    })
+  });
+
+  rangeSlider.noUiSlider.on('update', function (values) {
+    var maxPos = Math.max(values);
+    var pips = rangeSlider.querySelectorAll('.noUi-marker-horizontal.noUi-marker-sub');
+
+    if (!pips) {
+      return;
+    }
+
+    for (var i = 0; i < pips.length; i++) {
+
+      if (i <= maxPos - 1) {
+        pips[i].classList.add('form-range__accent');
+      } else {
+        pips[i].classList.remove('form-range__accent');
+      }
+    }
+
+  });
+
+})();
 
 'use strict';
 
