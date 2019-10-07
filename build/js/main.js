f90e894fb9c9c95aeae422c4bcff76a575212937
Artem Kartavtsev <arkartavtsev@live.ru>
2019-10-07 20:38:00 +0700

    Добавляет смену размера и семейства шрифта в версии для слабовидящих

--- build/js/main.js
+++ build/js/main.js
@@ -741,6 +741,9 @@
 
 (function () {
   var hideClass = 'js-hide';
+
+  var root = document.documentElement;
+
   var popup = document.querySelector('.special-popup');
   var btnsOpenPopup = document.querySelectorAll('.js-open-popup');
 
@@ -748,16 +751,34 @@
     var popupCloseBtn = popup.querySelector('.js-close-popup');
     var listsOptions = popup.querySelectorAll('.special-popup__list');
 
+
+    var toggleProperty = function (btn) {
+      var targetProperty = btn.dataset.property;
+      var targetValue = btn.dataset.value;
+
+      root.classList.forEach(function (item) {
+        if (new RegExp(targetProperty).test(item)) {
+          root.classList.remove(item);
+        }
+      });
+
+      root.classList.add(targetProperty + '--' + targetValue);
+    };
+
+
     listsOptions.forEach(function (list) {
       var btns = list.querySelectorAll('button');
 
       btns.forEach(function (btn) {
         btn.addEventListener('click', function (evt) {
           evt.preventDefault();
+
           btns.forEach(function (btn) {
             btn.classList.remove('active');
           });
           btn.classList.add('active');
+
+          toggleProperty(evt.currentTarget);
         });
       });
     });
@@ -780,7 +801,6 @@
       }
     });
   }
-
 })();
 
 'use strict';
--- source/js/modules/special.js
+++ source/js/modules/special.js
@@ -2,6 +2,9 @@
 
 (function () {
   var hideClass = 'js-hide';
+
+  var root = document.documentElement;
+
   var popup = document.querySelector('.special-popup');
   var btnsOpenPopup = document.querySelectorAll('.js-open-popup');
 
@@ -9,16 +12,34 @@
     var popupCloseBtn = popup.querySelector('.js-close-popup');
     var listsOptions = popup.querySelectorAll('.special-popup__list');
 
+
+    var toggleProperty = function (btn) {
+      var targetProperty = btn.dataset.property;
+      var targetValue = btn.dataset.value;
+
+      root.classList.forEach(function (item) {
+        if (new RegExp(targetProperty).test(item)) {
+          root.classList.remove(item);
+        }
+      });
+
+      root.classList.add(targetProperty + '--' + targetValue);
+    };
+
+
     listsOptions.forEach(function (list) {
       var btns = list.querySelectorAll('button');
 
       btns.forEach(function (btn) {
         btn.addEventListener('click', function (evt) {
           evt.preventDefault();
+
           btns.forEach(function (btn) {
             btn.classList.remove('active');
           });
           btn.classList.add('active');
+
+          toggleProperty(evt.currentTarget);
         });
       });
     });
@@ -41,5 +62,4 @@
       }
     });
   }
-
 })();
