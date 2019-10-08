7b49bb7f2d8d05f1b64a1528a276725eda791fdd
AnVP <amltrp@mail.ru>
2019-10-08 08:48:17 +0500

    Добавляет проверку инпута

--- build/js/main.js
+++ build/js/main.js
@@ -70,8 +70,9 @@
   }
   var inputCount = form.querySelector('#count-answer');
   var inputContainer = document.querySelector('.answers__count');
+  var countAll = document.querySelector('.answers__all-count');
 
-  if (!inputCount && !inputContainer) {
+  if (!inputCount && !inputContainer && !countAll) {
     return;
   }
 
@@ -88,6 +89,13 @@
     inputCount.value = inputCount.value.replace(/[^\+\d]/g, '');
     inputCount.focus();
     inputContainer.classList.add('answers__count--hover');
+
+    if (inputCount.value < 1) {
+      inputCount.value = 1;
+    } else if (inputCount.value > +countAll.textContent) {
+      inputCount.value = +countAll.textContent;
+    }
+
   };
 })();
 
