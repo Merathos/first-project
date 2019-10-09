67b8759d5d2a4a5a1630fbee4872615e692dc8ed
Artem Kartavtsev <arkartavtsev@live.ru>
2019-10-10 01:59:37 +0700

    Добавляет плавное сворачивание шапки на десктопе

--- source/js/modules/page-header.js
+++ source/js/modules/page-header.js
@@ -4,23 +4,28 @@
 (function () {
   var header = document.querySelector('.page-header');
 
-
   if (!header) {
     return;
   }
 
 
+  var headerBar = header.querySelector('.page-header__bar');
+
+
   var adjustHeaderAppearance = function () {
-    if (window.pageYOffset > 0) {
+    if (document.documentElement.clientWidth >= window.const.resolution.DESKTOP && window.pageYOffset > 0) {
       header.classList.add('page-header--above-content');
+      header.style.top = -headerBar.offsetHeight + 'px';
     } else {
       header.classList.remove('page-header--above-content');
+      header.style.top = 0;
     }
   };
 
   var adjustPageContentTopPadding = function () {
     document.body.style.paddingTop = header.offsetHeight + 'px';
   };
+
 
   var onWindowResize = function () {
     header.classList.remove('page-header--above-content');
