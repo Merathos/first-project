edb744fdafd17695d18c8d71d371d09f5ef9224e
Artem Kartavtsev <arkartavtsev@live.ru>
2019-10-11 20:33:27 +0700

    Добавляет кнопку сброса у селекта по голосованию, правит зарезание

--- build/js/main.js
+++ build/js/main.js
@@ -129,6 +129,35 @@
 
 
 (function () {
+  var filterVote = document.querySelector('.filter-vote');
+
+  if (!filterVote) {
+    return;
+  }
+
+
+  var deselectLabel = '<button type="button"><span class="visually-hidden">Сбросить фильтр по голосам</span><svg width="12" height="12" viewBox="0 0 12 12"><path d="M1 1L11 11M11 1L1 11" stroke-width="2"/></svg></button>';
+
+
+  var addSelect = function () {
+    return new window.SlimSelect({
+      select: '.filter-vote select',
+      showSearch: false,
+      allowDeselect: true,
+      placeholder: 'Голосовал',
+
+      deselectLabel: deselectLabel
+    });
+  };
+
+
+  addSelect();
+})();
+
+'use strict';
+
+
+(function () {
   var filter = document.querySelector('.filter');
 
 
@@ -160,7 +189,6 @@
   var Scrollbar = window.Scrollbar;
   var selectItem = document.querySelector('#projects-select');
   var categorySelect = document.querySelector('#category-select');
-  var voteSelect = document.querySelector('#vote-select');
 
   var addSelect = function (el) {
     var select = new window.SlimSelect({
@@ -191,11 +219,6 @@
   if (categorySelect) {
     addSelect(categorySelect);
   }
-
-  if (voteSelect) {
-    addSelect(voteSelect);
-  }
-
 })();
 
 'use strict';
