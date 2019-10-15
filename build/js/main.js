d19ffb5f569c75cd4e7f243485568c0a886fe341
AnVP <amltrp@mail.ru>
2019-10-15 20:39:44 +0500

    Добавляет правки

--- build/js/main.js
+++ build/js/main.js
@@ -951,6 +951,30 @@
 'use strict';
 
 (function () {
+  var windowWidth = document.documentElement.clientWidth;
+  var tabList = document.querySelector('.activity-tabs__list');
+
+  if (!tabList) {
+    return;
+  }
+
+  var tab = tabList.querySelector('.activity-tabs__item--active');
+
+  if (!tab) {
+    return;
+  }
+
+  var posX = tab.offsetLeft;
+
+  if (windowWidth < window.const.resolution.TABLET) {
+    tabList.scrollLeft = posX - 16;
+  }
+
+})();
+
+'use strict';
+
+(function () {
   var textContainers = document.querySelectorAll('.js-text-hidden');
   if (!textContainers.length) {
     return;
