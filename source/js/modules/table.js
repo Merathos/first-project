3ecb4ceba396a6d0d27016f64df96a6d6bebc6ef
AnVP <amltrp@mail.ru>
2019-10-25 17:48:11 +0500

    Добавляет вторую версию таблицы

--- source/js/modules/table.js
+++ source/js/modules/table.js
@@ -8,30 +8,8 @@
   }
 
   Scrollbar.init(tableContainer, {
-    continuousScrolling: false
-  });
-
-  var table = tableContainer.querySelector('.table-fixed__table tbody');
-  Scrollbar.init(table, {
-    continuousScrolling: false
-  });
-
-  var tablePopup = document.querySelector('.table-fixed--all-screen .table-fixed__table tbody');
-  if (!tablePopup) {
-    return;
-  }
-
-  Scrollbar.init(tablePopup, {
-    continuousScrolling: false
-  });
-
-  var tableCommentPopup = document.querySelector('.table-fixed--popup');
-  if (!tableCommentPopup) {
-    return;
-  }
-
-  Scrollbar.init(tableCommentPopup, {
-    continuousScrolling: false
+    continuousScrolling: false,
+    alwaysShowTracks: true
   });
 
 })();
