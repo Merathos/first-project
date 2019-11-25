43ab71e0f0d27b0b114729f60aa0a82fd2104f2a
Artem Kartavtsev <arkartavtsev@live.ru>
2019-11-26 02:27:22 +0700

    Добавляет компонент кнопки избранного

--- build/js/main.js
+++ build/js/main.js
@@ -188,12 +188,12 @@
     return new window.SlimSelect({
       select: el,
       showSearch: false,
+      showContent: 'down',
 
       placeholder: el.dataset.placeholderText ? el.dataset.placeholderText : '',
 
       allowDeselect: el.dataset.deselect ? true : false,
       deselectLabel: deselectLabel,
-      showContent: 'down',
 
       afterClose: function () {
         window.Scrollbar.destroy(document.querySelector('.form-select .ss-list'));
@@ -220,86 +220,91 @@
 
 
 (function () {
-  var popup = document.querySelector('.js-gallery-popup');
+    var popup = document.querySelector('.js-gallery-popup');
 
-  if (!popup) {
-    return;
-  }
-
-
-  var slider = popup.querySelector('.js-gallery-popup__frame');
-  var prevBtn = popup.querySelector('.js-gallery-popup__nav-btn--prev');
-  var nextBtn = popup.querySelector('.js-gallery-popup__nav-btn--next');
-
-  var description = popup.querySelector('.js-gallery-popup__photo-description');
-  var currentCounter = popup.querySelector('.js-gallery-popup__counter-current');
-  var totalCounter = popup.querySelector('.js-gallery-popup__counter-total');
-
-  var swiper = null;
-
-
-  var resetSlider = function () {
-    swiper.destroy();
-    slider.innerHTML = '';
-  };
-
-  var initializeSlider = function (target) {
-    var items = target.parentElement.parentElement.children;
-    var currentItemDescription = target.querySelector('.gallery__item-text');
-
-    var sliderTape = document.createElement('div');
-    sliderTape.classList.add('swiper-wrapper');
-
-    Array.prototype.forEach.call(items, function (item) {
-      var itemPhoto = item.querySelector('.gallery__item');
-      var element = itemPhoto.cloneNode(true);
-      element.classList.add('swiper-slide');
-
-      sliderTape.appendChild(element);
-    });
-
-    slider.appendChild(sliderTape);
-
-    swiper = new window.Swiper(slider, {
-      initialSlide: +target.dataset.index,
-      slidesPerView: 1,
-      watchOverflow: true,
-      grabCursor: true,
-      loop: true,
-
-      navigation: {
-        prevEl: prevBtn,
-        nextEl: nextBtn,
-      },
-
-      on: {
-        slideChange: function () {
-          description.textContent = items[this.realIndex].querySelector('.gallery__item-text').textContent;
-          currentCounter.textContent = +this.realIndex + 1;
-        }
-      }
-    });
-
-    description.textContent = currentItemDescription.textContent;
-    currentCounter.textContent = +target.dataset.index + 1;
-    totalCounter.textContent = items.length;
-  };
-
-
-  var onDocumentClick = function (evt) {
-    if (evt.target.classList.contains('js-gallery__item-link')) {
-      evt.preventDefault();
-
-      if (swiper) {
-        resetSlider();
-      }
-
-      window.openPopup(popup);
-      initializeSlider(evt.target);
+    if (!popup) {
+        return;
     }
-  };
 
-  document.addEventListener('click', onDocumentClick);
+    var hideClass = 'd-none';
+    var slider = popup.querySelector('.js-gallery-popup__frame');
+    var prevBtn = popup.querySelector('.js-gallery-popup__nav-btn--prev');
+    var nextBtn = popup.querySelector('.js-gallery-popup__nav-btn--next');
+
+    var description = popup.querySelector('.js-gallery-popup__photo-description');
+    var currentCounter = popup.querySelector('.js-gallery-popup__counter-current');
+    var totalCounter = popup.querySelector('.js-gallery-popup__counter-total');
+    var popupBarBottom = popup.querySelector('.popup__bar--bottom');
+
+    var swiper = null;
+
+
+    var resetSlider = function () {
+        swiper.destroy();
+        slider.innerHTML = '';
+    };
+
+    var initializeSlider = function (target) {
+        var items = target.parentElement.parentElement.children;
+        var currentItemDescription = target.querySelector('.gallery__item-text');
+
+        var sliderTape = document.createElement('div');
+        sliderTape.classList.add('swiper-wrapper');
+
+        Array.prototype.forEach.call(items, function (item) {
+            var itemPhoto = item.querySelector('.gallery__item');
+            var element = itemPhoto.cloneNode(true);
+            element.classList.add('swiper-slide');
+
+            sliderTape.appendChild(element);
+        });
+
+        slider.appendChild(sliderTape);
+
+        swiper = new window.Swiper(slider, {
+            initialSlide: +target.dataset.index,
+            slidesPerView: 1,
+            watchOverflow: true,
+            grabCursor: true,
+            loop: true,
+
+            navigation: {
+                prevEl: prevBtn,
+                nextEl: nextBtn,
+            },
+
+            on: {
+                slideChange: function () {
+                    description.textContent = items[this.realIndex].querySelector('.gallery__item-text').textContent;
+                    currentCounter.textContent = +this.realIndex + 1;
+                }
+            }
+        });
+
+        description.textContent = currentItemDescription.textContent;
+        currentCounter.textContent = +target.dataset.index + 1;
+        totalCounter.textContent = items.length;
+
+        if (items.length <= 1) {
+          popupBarBottom.classList.add(hideClass);
+        }
+    };
+
+
+    var onDocumentClick = function (evt) {
+        if (evt.target.classList.contains('js-gallery__item-link')) {
+            evt.preventDefault();
+
+            if (swiper) {
+                resetSlider();
+            }
+
+            window.openPopup(popup);
+            initializeSlider(evt.target);
+        }
+    };
+
+    document.addEventListener('click', onDocumentClick);
 })();
 
 'use strict';
