d28aa441c8b98f779b3bf78ac0d16d7cce851b61
fromaliga <grechino@htmlacademy.ru>
2020-08-26 17:09:54 +0300

    Добавил информационный баннер; Добавил вариацию главной страницы с баннером без заголовка

--- build/js/main.js
+++ build/js/main.js
@@ -358,6 +358,25 @@
   });
 })();
 
+'use strict'
+;(function () {
+  var infoBanner = document.querySelectorAll('.info-banner')
+
+  if (infoBanner.length > 0) {
+    infoBanner.forEach(function (banner) {
+      var bannerText = banner.querySelector('.info-banner__text')
+      var bannerToggleTextButton = banner.querySelector('.info-banner__button')
+
+      if (bannerText && bannerToggleTextButton) {
+        bannerToggleTextButton.addEventListener('click', function (e) {
+          e.currentTarget.classList.toggle('opened')
+          bannerText.classList.toggle('opened')
+        })
+      }
+    })
+  }
+})()
+
 'use strict';
 
 
