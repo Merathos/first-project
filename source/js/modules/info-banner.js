4c2fe50ef61cefbc7df5aefb95599e01cb2835d5
fromaliga <grechino@htmlacademy.ru>
2020-08-27 13:21:40 +0300

    обновил вёрстку инфо  баннера в соответсвии с клиентской

--- source/js/modules/info-banner.js
+++ source/js/modules/info-banner.js
@@ -8,10 +8,31 @@
       var bannerToggleTextButton = banner.querySelector('.info-banner__button')
 
       if (bannerText && bannerToggleTextButton) {
-        bannerToggleTextButton.addEventListener('click', function (e) {
-          e.currentTarget.classList.toggle('opened')
-          bannerText.classList.toggle('opened')
-        })
+        // Количество строк текста
+        var bannerTextHeight = bannerText.getBoundingClientRect().height
+        var bannerTextLineHeight = +getComputedStyle(
+          bannerText
+        ).lineHeight.replace('px', '')
+        var bannerTextLines = Math.ceil(bannerTextHeight / bannerTextLineHeight)
+
+        // Максимально допустимое количество строк
+        var maxLinesAmount = 3
+
+        // Если в блоке есть заголовок, то максимальное количество строк отличается по макету, но в ТЗ речь про 3 при любых условиях
+        if (banner.querySelector('.info-banner__title')) {
+          maxLinesAmount = 2
+        }
+
+        if (bannerTextLines > maxLinesAmount) {
+          banner.classList.add('info-banner_enough-lines_true')
+
+          bannerToggleTextButton.addEventListener('click', function (e) {
+            e.currentTarget.classList.toggle('opened')
+            bannerText.classList.toggle('opened')
+          })
+        } else {
+          banner.classList.add('info-banner_enough-lines_false')
+        }
       }
     })
   }
