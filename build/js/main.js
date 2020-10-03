7def389d84d27fa718a65c7e9cc953055ccbf4bd
Serge Krykov <sergkryk@Serges-iMac.local>
2020-10-03 08:12:12 +0300

    добавляет логику в комментарии

--- build/js/main.js
+++ build/js/main.js
@@ -557,6 +557,28 @@
 
 
 (function () {
+  var toggle = document.querySelector('.js-moderator-comments');
+
+  if (toggle) {
+    var container = toggle.parentElement;
+    var containerHeight = container.scrollHeight;
+
+    toggle.addEventListener('click', function () {
+      if (!container.classList.contains('comments-tab--opened')) {
+        container.classList.add('comments-tab--opened');
+        container.style.height = containerHeight + 'px';
+      } else {
+        container.classList.remove('comments-tab--opened');
+        container.removeAttribute('style');
+      }
+    });
+  }
+})();
+
+'use strict';
+
+
+(function () {
   var header = document.querySelector('.js-page-header');
 
   if (!header) {
