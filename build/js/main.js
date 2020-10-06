1f88b63a4a70c87e624152fca8d06411618c3c3a
Vladimir <karetnikov.v.e@gmail.com>
2020-10-06 11:39:33 +0300

    fixes

--- build/js/main.js
+++ build/js/main.js
@@ -980,6 +980,24 @@
 })();
 
 'use strict';
+
+
+(function () {
+    if (document.querySelector('.proposal-winners__show-map-btn')) {
+        var mapBtn = document.querySelector('.proposal-winners__show-map-btn');
+        var map = document.querySelector('.proposal-winners__map');
+        var closeMapBtn = document.querySelector('.map-popup__close-btn');
+
+        mapBtn.addEventListener('click', function () {
+            map.classList.add('proposal-winners__map--show');
+        });
+
+        closeMapBtn.addEventListener('click', function () {
+            map.classList.remove('proposal-winners__map--show');
+        });
+    }
+})();
+'use strict';
 (function () {
   var container = document.querySelector('.js-upload-image-container');
 
