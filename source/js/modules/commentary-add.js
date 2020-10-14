f783a69ab8ac89b3f74905ff21553bfa2ba3d188
Vladimir <karetnikov.v.e@gmail.com>
2020-10-14 20:41:00 +0300

    Добавил валидацию

---
+++ source/js/modules/commentary-add.js
@@ -0,0 +1,25 @@
+'use strict';
+
+(function () {
+    if (document.querySelector('.commentary-add')) {
+        var textareaContainer = document.querySelector('.commentary-add__textarea');
+        var textarea = textareaContainer.querySelector('#commentary-textarea');
+        var errorField = textareaContainer.querySelector('#textarea-error-field');
+
+        textarea.addEventListener('change', function () {
+            if (textarea.value.length >= 500) {
+                textareaContainer.classList.add('commentary-add__textarea--warning');
+                errorField.textContent = 'Осталось символов: ' + Math.abs(1000 - textarea.value.length);
+            } else {
+                textareaContainer.classList.remove('commentary-add__textarea--warning');
+            }
+
+            if (textarea.value.length >= 1000) {
+                textareaContainer.classList.add('commentary-add__textarea--error');
+                errorField.textContent = 'Вы превысили лимит символов на ' + Math.abs(1000 - textarea.value.length);
+            } else {
+                textareaContainer.classList.remove('commentary-add__textarea--error');
+            }
+        });
+    }
+})();
