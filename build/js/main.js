7eb87699dee8c123add19816c40351dddcc7bc87
fromaliga <grecino@htmlacademy.ru>
2020-10-15 13:24:57 +0300

    выравнивает изменения с master-веткой в репозитории клиента

--- build/js/main.js
+++ build/js/main.js
@@ -423,58 +423,45 @@
   });
 })();
 
-'use strict';
-(function () {
-  var infoBanner = document.querySelectorAll('.info-banner');
+'use strict'
+;(function () {
+  var infoBanner = document.querySelectorAll('.info-banner')
 
   if (infoBanner.length > 0) {
     infoBanner.forEach(function (banner) {
-      var bannerText = banner.querySelector('.info-banner__text');
-      var bannerToggleTextButton = banner.querySelector('.info-banner__button');
+      var bannerText = banner.querySelector('.info-banner__text')
+      var bannerToggleTextButton = banner.querySelector('.info-banner__button')
 
       if (bannerText && bannerToggleTextButton) {
-
-        var textSpan = bannerToggleTextButton.querySelector('.info-banner-button__text');
-        var changeButtonText = function () {
-          if (bannerToggleTextButton.classList.contains('opened')) {
-            textSpan.textContent = 'Свернуть';
-          } else {
-            textSpan.textContent = 'Развернуть';
-          }
-        };
         // Количество строк текста
-        var bannerTextHeight = bannerText.getBoundingClientRect().height;
+        var bannerTextHeight = bannerText.getBoundingClientRect().height
         var bannerTextLineHeight = +getComputedStyle(
-            bannerText
-        ).lineHeight.replace('px', '');
-        var bannerTextLines = Math.ceil(
-            bannerTextHeight / bannerTextLineHeight
-        );
+          bannerText
+        ).lineHeight.replace('px', '')
+        var bannerTextLines = Math.ceil(bannerTextHeight / bannerTextLineHeight)
 
         // Максимально допустимое количество строк
-        var maxLinesAmount = 3;
+        var maxLinesAmount = 3
 
         // Если в блоке есть заголовок, то максимальное количество строк отличается по макету, но в ТЗ речь про 3 при любых условиях
         if (banner.querySelector('.info-banner__title')) {
-          maxLinesAmount = 2;
+          maxLinesAmount = 2
         }
 
         if (bannerTextLines > maxLinesAmount) {
-          banner.classList.add('info-banner_enough-lines_true');
+          banner.classList.add('info-banner_enough-lines_true')
 
           bannerToggleTextButton.addEventListener('click', function (e) {
-            e.currentTarget.classList.toggle('opened');
-            bannerText.classList.toggle('opened');
-            changeButtonText();
-          });
+            e.currentTarget.classList.toggle('opened')
+            bannerText.classList.toggle('opened')
+          })
         } else {
-          banner.classList.add('info-banner_enough-lines_false');
-          changeButtonText();
+          banner.classList.add('info-banner_enough-lines_false')
         }
       }
-    });
+    })
   }
-})();
+})()
 
 'use strict';
 
@@ -1655,7 +1642,7 @@ document.querySelectorAll('.js-poll-range').forEach(function (item) {
     });
   }
 
-  
+
   var mySwiper = new Swiper('.swiper-container', {
     speed: 400,
     autoplay: {
@@ -1683,7 +1670,7 @@ document.querySelectorAll('.js-poll-range').forEach(function (item) {
             onPageClick: function(pageNumber, event) {
               mySwiper.slideTo(pageNumber - 1,400,false);
             },
-          });   
+          });
         if(i === widgetPaginationEl){
           i = 1;
         }
@@ -1691,7 +1678,7 @@ document.querySelectorAll('.js-poll-range').forEach(function (item) {
     },
   });
 
-  
+
   var mySwiper = document.querySelector('.swiper-container').swiper;
 
 
@@ -1744,7 +1731,7 @@ document.querySelectorAll('.js-poll-range').forEach(function (item) {
                   mySwiper.slideTo(pageNumber - 1,400,false);
                 },
               });
-             
+
               if(i === widgetPaginationEl){
                 i = 1;
               }
@@ -1756,7 +1743,7 @@ document.querySelectorAll('.js-poll-range').forEach(function (item) {
       } else {
         this.parentElement.append(error);
         this.style.border = '1px solid #F3A8A8';
-        error.innerHTML = "Количество элементов на странице не может быть 0";} 
+        error.innerHTML = "Количество элементов на странице не может быть 0";}
 
     });
   }
@@ -1806,7 +1793,7 @@ document.querySelectorAll('.js-poll-range').forEach(function (item) {
                   mySwiper.slideTo(pageNumber - 1,400,false);
                 },
               });
-             
+
               if(i === widgetPaginationEl){
                 i = 1;
               }
@@ -1818,7 +1805,7 @@ document.querySelectorAll('.js-poll-range').forEach(function (item) {
         this.parentElement.append(error);
         this.style.border = '1px solid #F3A8A8';
         error.innerHTML = "Общее количество элементов не может быть 0";
-      } 
+      }
     });
   }
 
@@ -1864,7 +1851,7 @@ document.querySelectorAll('.js-poll-range').forEach(function (item) {
                   mySwiper.slideTo(pageNumber - 1,400,false);
                 },
               });
-             
+
               if(i === widgetPaginationEl){
                 i = 1;
               }
@@ -1875,7 +1862,7 @@ document.querySelectorAll('.js-poll-range').forEach(function (item) {
       } else {
         this.parentElement.append(error);
         this.style.border = '1px solid #F3A8A8';
-        error.innerHTML = "Время не может быть пустым";} 
+        error.innerHTML = "Время не может быть пустым";}
     });
   }
 
@@ -1922,7 +1909,7 @@ document.querySelectorAll('.js-poll-range').forEach(function (item) {
                   mySwiper.slideTo(pageNumber - 1,400,false);
                 },
               });
-             
+
               if(i === widgetPaginationEl){
                 i = 1;
               }
@@ -1946,7 +1933,7 @@ document.querySelectorAll('.js-poll-range').forEach(function (item) {
 (function () {
   var WidgetGetCode = document.querySelector('.js-widget-open');
   var WidgetPopup = document.querySelector('.widget-popup');
-  
+
   if(WidgetGetCode){
     WidgetGetCode.addEventListener('click',function(){
       WidgetPopup.classList.add('popup--shown');
@@ -2053,5 +2040,5 @@ document.querySelectorAll('.js-poll-range').forEach(function (item) {
   if (widgetSelectPreview) {
     addSelectPreview(widgetSelectPreview);
   }
-  
+
 })();
