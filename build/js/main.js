ebfb49dccc88c413c6c21f73b0c1a46273001cb9
Aleksey Kopasov <alesey.k1@gmail.com>
2020-11-22 17:33:17 +0300

    Вносит правки

--- build/js/main.js
+++ build/js/main.js
@@ -1021,24 +1021,46 @@
 
 (function () {
   var container = document.querySelectorAll('.js-like-counter');
+  var likesContainerClass ='js_likes_container';
 
   if (container.length) {
     container.forEach(function (element) {
-      var counter = element.querySelector('span');
-      element.addEventListener('click', function () {
-        var value = parseInt(counter.textContent, 10);
-
-        if (element.classList.contains('liked')) {
-          element.classList.remove('liked');
-          value -= 1;
-          counter.textContent = value;
-        } else {
-          element.classList.add('liked');
-          value++;
-          counter.textContent = value;
-        }
-      });
+      element.addEventListener('click', onBtnClick);
     });
+  }
+
+  function onBtnClick(evt) {
+    var target = evt.currentTarget;
+
+    if (target.parentElement.querySelector('.liked')) {
+      return;
+    }
+
+    var counter = target.querySelector('span');
+    var value = parseInt(counter.textContent, 10);
+
+    if (target.classList.contains('liked')) {
+      target.classList.remove('liked');
+      value -= 1;
+      counter.textContent = value;
+      checkLikedClassFromAdjacentElement(target);
+    } else {
+      target.classList.add('liked');
+      value++;
+      counter.textContent = value;
+      checkLikedClassFromAdjacentElement(target);
+    }
+  }
+
+  function checkLikedClassFromAdjacentElement(el) {
+    var parent = el.parentElement;
+    if (parent.classList.contains(likesContainerClass)) {
+      if (parent.querySelector('button:not(.liked)')) {
+        parent.querySelectorAll('button').forEach(function (btn) {
+          btn.removeEventListener('click', onBtnClick)
+        });
+      }
+    }
   }
 })();
 
