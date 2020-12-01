10dd571a78ca07eaad901f06940a044e64882203
Aleksey Kopasov <alesey.k1@gmail.com>
2020-12-01 15:54:14 +0300

    Исправляет ошибки в консоли

--- source/js/modules/range.js
+++ source/js/modules/range.js
@@ -1,59 +1,61 @@
 'use strict';
 
 function initRangeSlider(rangeSlider) {
-    if (!rangeSlider) {
-        return;
-    }
+  if (!rangeSlider) {
+    return;
+  }
 
-    var input = document.getElementById(rangeSlider.dataset.input);
+  var input = document.getElementById(rangeSlider.dataset.input);
 
+  if (input) {
     var min = parseInt(rangeSlider.dataset.min);
     var max = parseInt(rangeSlider.dataset.max);
     var start = input.value ? input.value : max / 2;
     window.noUiSlider.create(rangeSlider, {
-        start: start,
-        behaviour: 'snap',
-        connect: 'lower',
-        step: parseInt(rangeSlider.dataset.step),
-        range: {
-            'min': [min],
-            'max': [max]
-        },
-        pips: {
-            mode: 'steps',
-            stepped: true,
-            density: 4
-        },
-        tooltips: true,
-        format: window.wNumb({
-            decimals: 0,
-        })
+      start: start,
+      behaviour: 'snap',
+      connect: 'lower',
+      step: parseInt(rangeSlider.dataset.step),
+      range: {
+        'min': [min],
+        'max': [max]
+      },
+      pips: {
+        mode: 'steps',
+        stepped: true,
+        density: 4
+      },
+      tooltips: true,
+      format: window.wNumb({
+        decimals: 0,
+      })
     });
 
     rangeSlider.noUiSlider.on('update', function (values, handle) {
 
-        var value = values[handle];
-        var maxPos = Math.max(values);
-        var pips = rangeSlider.querySelectorAll('.noUi-marker-horizontal.noUi-marker-sub');
+      var value = values[handle];
+      var maxPos = Math.max(values);
+      var pips = rangeSlider.querySelectorAll('.noUi-marker-horizontal.noUi-marker-sub');
 
-        input.value = value;
+      input.value = value;
 
-        if (!pips) {
-            return;
+      if (!pips) {
+        return;
+      }
+
+      for (var i = 0; i < pips.length; i++) {
+
+        if (i <= maxPos - 2) {
+          pips[i].classList.add('form-range__accent');
+        } else {
+          pips[i].classList.remove('form-range__accent');
         }
-
-        for (var i = 0; i < pips.length; i++) {
-
-            if (i <= maxPos - 2) {
-                pips[i].classList.add('form-range__accent');
-            } else {
-                pips[i].classList.remove('form-range__accent');
-            }
-        }
+      }
 
     });
+  }
 }
 document.querySelectorAll('.js-poll-range').forEach(function (item) {
-    initRangeSlider(item);
+  initRangeSlider(item);
 });
 
