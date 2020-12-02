26271103f98d16facb8b2178ab16903987259a6d
Oleg Oschepkov <o_oleg_o@inbox.ru>
2020-12-02 18:37:49 +0400

    Fix for map

--- build/js/main.js
+++ build/js/main.js
@@ -989,6 +989,125 @@
           });
     });
 
+    var MyBalloonLayout = ymaps.templateLayoutFactory.createClass(
+      '<div class="popover right">' +
+      '<button type="button" aria-label="Закрыть информацию" class="close"></button>' +
+      '<div class="arrow"></div>' +
+      '<div class="popover-inner">' +
+      '$[[options.contentLayout observeSize minWidth=300 maxWidth=300 maxHeight=500]]' +
+      '</div>' +
+      '</div>', {
+        /**
+         * Строит экземпляр макета на основе шаблона и добавляет его в родительский HTML-элемент.
+         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/layout.templateBased.Base.xml#build
+         * @function
+         * @name build
+         */
+        build: function () {
+          this.constructor.superclass.build.call(this);
+
+          this._$element = $('.popover', this.getParentElement());
+
+          this.applyElementOffset();
+
+          this._$element.find('.close')
+            .on('click', $.proxy(this.onCloseClick, this));
+        },
+
+        /**
+         * Удаляет содержимое макета из DOM.
+         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/layout.templateBased.Base.xml#clear
+         * @function
+         * @name clear
+         */
+        clear: function () {
+          this._$element.find('.close')
+            .off('click');
+
+          this.constructor.superclass.clear.call(this);
+        },
+
+        /**
+         * Метод будет вызван системой шаблонов АПИ при изменении размеров вложенного макета.
+         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/IBalloonLayout.xml#event-userclose
+         * @function
+         * @name onSublayoutSizeChange
+         */
+        onSublayoutSizeChange: function () {
+          MyBalloonLayout.superclass.onSublayoutSizeChange.apply(this, arguments);
+
+          if(!this._isElement(this._$element)) {
+            return;
+          }
+
+          this.applyElementOffset();
+
+          this.events.fire('shapechange');
+        },
+
+        /**
+         * Сдвигаем балун, чтобы "хвостик" указывал на точку привязки.
+         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/IBalloonLayout.xml#event-userclose
+         * @function
+         * @name applyElementOffset
+         */
+        applyElementOffset: function () {
+          this._$element.css({
+            top: -(this._$element[0].offsetHeight/2)
+          });
+        },
+
+        /**
+         * Закрывает балун при клике на крестик, кидая событие "userclose" на макете.
+         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/IBalloonLayout.xml#event-userclose
+         * @function
+         * @name onCloseClick
+         */
+        onCloseClick: function (e) {
+          e.preventDefault();
+
+          this.events.fire('userclose');
+        },
+
+        /**
+         * Используется для автопозиционирования (balloonAutoPan).
+         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/ILayout.xml#getClientBounds
+         * @function
+         * @name getClientBounds
+         * @returns {Number[][]} Координаты левого верхнего и правого нижнего углов шаблона относительно точки привязки.
+         */
+        getShape: function () {
+          if(!this._isElement(this._$element)) {
+            return MyBalloonLayout.superclass.getShape.call(this);
+          }
+
+          var position = this._$element.position();
+
+          return new ymaps.shape.Rectangle(new ymaps.geometry.pixel.Rectangle([
+            [position.left, position.top], [
+              position.left + this._$element[0].offsetWidth,
+              position.top + this._$element[0].offsetHeight + this._$element.find('.arrow')[0].offsetHeight
+            ]
+          ]));
+        },
+
+        /**
+         * Проверяем наличие элемента (в ИЕ и Опере его еще может не быть).
+         * @function
+         * @private
+         * @name _isElement
+         * @param {jQuery} [element] Элемент.
+         * @returns {Boolean} Флаг наличия.
+         */
+        _isElement: function (element) {
+          return element && element[0] && element.find('.arrow')[0];
+        }
+      });
+
+    // var MyBalloonContentLayout = ymaps.templateLayoutFactory.createClass(
+    //   '<h3 class="popover-title">$[properties.balloonHeader]</h3>' +
+    //   '<div class="popover-content">$[properties.balloonContent]</div>'
+    // );
 
     var dataLink = $mapBlock.data('link');
 
@@ -998,11 +1117,14 @@
         for (var i=0; i< myObjects.length; i++) {
           if(!myObjects[i].geometry._coordPath) {
             myObjects[i].options.set({
+              balloonLayout: MyBalloonLayout,
+              // balloonContentLayout: MyBalloonContentLayout,
               iconLayout: 'default#image',
               iconImageHref: 'img/pin.svg',
               iconImageSize: [28, 40],
               hideIconOnBalloonOpen: false,
-              balloonPanelMaxMapArea: 0
+              balloonPanelMaxMapArea: 0,
+              balloonOffset: [43, -27]
             })
             clusterer.add(myObjects[i]);
           } else {
