c9ac1b1bb10573bcdae6b245a94261b0cc29c1a2
Aleksey Kopasov <alesey.k1@gmail.com>
2020-12-02 21:19:23 +0300

    Вносит правки

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,27 @@
 'use strict';
 
+(function () {
+  var buttonsFavorite = document.querySelectorAll('.js-button-favorive');
+
+  if (!buttonsFavorite) {
+    return;
+  }
+
+  var activeClass = 'button-star--active';
+
+  function favoriteClickHanler(evt) {
+    var target = evt.target;
+
+    target.classList.toggle(activeClass);
+  }
+
+  buttonsFavorite.forEach(function(button) {
+    button.addEventListener('click', favoriteClickHanler);
+  });
+})();
+
+'use strict';
+
 
 (function () {
 
