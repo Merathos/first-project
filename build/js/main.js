646d7c8667f6428f509fd57f4e4cdb837d8b67d4
sergkryk <sergkryk@ya.ru>
2021-03-04 22:18:46 +0300

    добавляет логику для раскрывашки

--- build/js/main.js
+++ build/js/main.js
@@ -293,6 +293,34 @@
 })();
 
 'use strict';
+
+(function () {
+  var togglers = document.querySelectorAll('.js-collapse-button');
+
+  if (togglers.length) {
+    togglers.forEach(function (toggler) {
+      var content = toggler.parentElement.querySelector('.js-collapse-content');
+      content.style.setProperty('--height', content.scrollHeight + 'px');
+      var text = toggler.querySelector('span');
+
+      var collapseContent = function () {
+        if (content.getAttribute('data-collapsed') === 'false') {
+          content.setAttribute('data-collapsed', 'true'); // mark the section as "currently collapsed"
+          text.textContent = 'Контакты';
+          toggler.classList.remove('collapsed');
+        } else {
+          content.setAttribute('data-collapsed', 'false'); // mark the section as "currently not collapsed"
+          text.textContent = 'Свернуть';
+          toggler.classList.add('collapsed');
+        }
+      };
+      collapseContent();
+      toggler.addEventListener('click', collapseContent);
+    });
+  }
+})();
+
+'use strict';
 (function () {
   var container = document.querySelector('.js-upload-file-container');
 
