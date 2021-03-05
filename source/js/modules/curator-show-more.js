920b1e532bcb81e51fca715b441a30164c603825
sergkryk <sergkryk@ya.ru>
2021-03-05 06:56:51 +0300

    исправляет недоработки

--- source/js/modules/curator-show-more.js
+++ source/js/modules/curator-show-more.js
@@ -2,20 +2,52 @@
 
 (function () {
   var togglers = document.querySelectorAll('.js-collapse-button');
+  var collapsedContents = document.querySelectorAll('.js-collapse-content');
+
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+      // eslint-disable-next-line no-invalid-this
+      var context = this;
+      var args = arguments;
+      clearTimeout(timeout);
+      timeout = setTimeout(function () {
+        timeout = null;
+        if (!immediate) {
+          func.apply(context, args);
+        }
+      }, wait);
+      if (immediate && !timeout) {
+        func.apply(context, args);
+      }
+    };
+  }
+
+  if (collapsedContents.length) {
+    var setWidthVariable = debounce(function () {
+      collapsedContents.forEach(function (el) {
+        el.style.setProperty('--height', el.scrollHeight + 'px');
+      });
+    }, 250);
+
+    setWidthVariable();
+    window.addEventListener('resize', function () {
+      setWidthVariable();
+    });
+  }
 
   if (togglers.length) {
     togglers.forEach(function (toggler) {
       var content = toggler.parentElement.querySelector('.js-collapse-content');
-      content.style.setProperty('--height', content.scrollHeight + 'px');
       var text = toggler.querySelector('span');
 
       var collapseContent = function () {
         if (content.getAttribute('data-collapsed') === 'false') {
-          content.setAttribute('data-collapsed', 'true'); // mark the section as "currently collapsed"
+          content.setAttribute('data-collapsed', 'true');
           text.textContent = 'Контакты';
           toggler.classList.remove('collapsed');
         } else {
-          content.setAttribute('data-collapsed', 'false'); // mark the section as "currently not collapsed"
+          content.setAttribute('data-collapsed', 'false');
           text.textContent = 'Свернуть';
           toggler.classList.add('collapsed');
         }
