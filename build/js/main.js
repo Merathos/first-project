43245c38f1c25a61828cf7d33e88ed610e55779c
sergkryk <sergkryk@ya.ru>
2021-05-21 07:13:54 +0300

    добавляет радио кнопки в page-nav

--- build/js/main.js
+++ build/js/main.js
@@ -1993,6 +1993,27 @@
 
 'use strict';
 
+(function () {
+  var links = document.querySelectorAll('a[data-radio]');
+
+  if (links.length) {
+    var setChecked = function (attr) {
+      var radioInputToCheck = document.querySelector('input#' + attr);
+      if (radioInputToCheck) {
+        radioInputToCheck.checked = true;
+      }
+    };
+
+    links.forEach(function (link) {
+      link.addEventListener('click', function () {
+        setChecked(link.getAttribute('data-radio'));
+      });
+    });
+  }
+})();
+
+'use strict';
+
 
 (function () {
   var popups = document.querySelectorAll('.js-popup');
