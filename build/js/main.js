909b5f7251f4a7c459c9f4cbb02024a44bcb2c2c
mariata06 <slinkywizard@mail.ru>
2022-01-12 00:44:47 +0300

    Добавляет страницу после голосования, дополняет проекты, добавляет галерею, аккордеон

--- build/js/main.js
+++ build/js/main.js
@@ -1016,6 +1016,59 @@
 })();
 
 'use strict';
+(function () {
+  var projectAkordeon = document.querySelectorAll('.proposal__project');
+
+  if (projectAkordeon.length > 0) {
+    projectAkordeon.forEach(function (project) {
+      var projectText = project.querySelector('.proposal__project-text');
+      var projectToggleTextButton = project.querySelector('.button-details');
+
+      if (projectText && projectToggleTextButton) {
+
+        var textSpan = projectToggleTextButton.querySelector('.button-details__text');
+        var changeButtonText = function () {
+          if (projectToggleTextButton.classList.contains('opened')) {
+            textSpan.textContent = 'Свернуть';
+          } else {
+            textSpan.textContent = 'Подробнее';
+          }
+        };
+        // Количество строк текста
+        var projectTextHeight = projectText.getBoundingClientRect().height;
+        var projectTextLineHeight = +getComputedStyle(
+            projectText
+        ).lineHeight.replace('px', '');
+        var projectTextLines = Math.ceil(
+            projectTextHeight / projectTextLineHeight
+        );
+
+        // Максимально допустимое количество строк
+        var maxLinesAmount = 4;
+
+        // Если в блоке есть заголовок, то максимальное количество строк отличается по макету, но в ТЗ речь про 3 при любых условиях
+        if (project.querySelector('.proposal__project-title')) {
+          maxLinesAmount = 2;
+        }
+
+        if (projectTextLines > maxLinesAmount) {
+          project.classList.add('project_enough-lines_true');
+
+          projectToggleTextButton.addEventListener('click', function (e) {
+            e.currentTarget.classList.toggle('opened');
+            projectText.classList.toggle('opened');
+            changeButtonText();
+          });
+        } else {
+          project.classList.add('project_enough-lines_false');
+          changeButtonText();
+        }
+      }
+    });
+  }
+})();
+
+'use strict';
 
 
 (function () {
