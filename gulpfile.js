9dfa49949951451d943f97cc7845808a84113abf
Oleg Oschepkov <o_oleg_o@inbox.ru>
2022-08-22 15:57:19 +0400

    stylesV2 ready for test

--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,21 @@ gulp.task('style', () => gulp.src('source/sass/style.scss')
   .pipe(server.stream())
 );
 
+gulp.task('styleV2', () => gulp.src('source/sass/style-v2.scss')
+  .pipe(plumber())
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(postcss([
+    autoprefixer()
+  ]))
+  .pipe(gulp.dest('build/css'))
+  .pipe(minify())
+  .pipe(rename('style2.min.css'))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('build/css'))
+  .pipe(server.stream())
+);
+
 gulp.task('js', () => gulp.src(['source/js/**/*.js', '!source/js/plugins/*.js'])
   .pipe(plumber())
   .pipe(concat('main.js'))
@@ -112,10 +127,10 @@ gulp.task('serve', () => {
 
   gulp.watch('source/img/**/*.{gif,png,jpg,svg,webp}', gulp.series('images', 'webp'));
   gulp.watch('source/img/sprite/*.svg', gulp.series('sprite'));
-  gulp.watch('source/sass/**/*.scss', gulp.series('style'));
+  gulp.watch('source/sass/**/*.scss', gulp.series('styleV2'));
   gulp.watch('source/**/*.html', gulp.series('html')).on('change', server.reload);
   gulp.watch('source/js/**/*.js', gulp.series('jsPlugins', 'js')).on('change', server.reload);
   gulp.watch('source/json/**', gulp.series('copyJson')).on('change', server.reload);
 });
 
-gulp.task('build', gulp.series('clean', 'images', 'webp', 'sprite', 'copy', 'style', 'html', 'js', 'jsPlugins'));
+gulp.task('build', gulp.series('clean', 'images', 'webp', 'sprite', 'copy', 'style', 'styleV2', 'html', 'js', 'jsPlugins'));
